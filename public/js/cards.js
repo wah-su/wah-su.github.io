1b0fa51ba58e2f353a94b1b3f4936a51f3ddda10
Kentai Radiquum <kentai.waah@gmail.com>
2024-06-25 03:55:35 +0500

    refactor: update Hosting Style. Update card style.

--- public/js/cards.js
+++ public/js/cards.js
@@ -61,7 +61,7 @@ function renderCards() {
                   </h5>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${cards[index].description}
                   </p>
-                  <div class="flex gap-1 items-center justify-center flex-wrap lg:flex-nowrap lg:justify-start">
+                  <div class="flex gap-1 items-center justify-center flex-wrap">
                       <a href="${cards[index].about}" target="_blank"
                           class="w-full inline-flex justify-center items-center py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                           Read more
@@ -81,7 +81,7 @@ function renderCards() {
 
     const element = document.createElement("div");
     element.className =
-      "max-w-[298px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
+      "max-w-[324px] sm:max-w-[275px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
     element.innerHTML = template;
 
     services.appendChild(element);
